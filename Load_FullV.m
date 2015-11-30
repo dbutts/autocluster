a5c80370fd051bcd97936c556aa80f591576e74b
James <james@CA1.(none)>
2015-11-30 14:57:22 -0500

    adding comments, reorganizing for clarity

--- Load_FullV.m
+++ Load_FullV.m
@@ -1,7 +1,20 @@
 function [V,Vtime,Fs] = Load_FullV(name, add_Vmean, filt_cutoff,use_chs)
+% [V,Vtime,Fs] = Load_FullV(name, <add_Vmean>, <filt_cutoff>, <use_chs>)
+% load voltage data from file and apply desired preprocessing
+% INPUTS: 
+%   name: file name containing voltage data
+%   add_Vmean (default true): Whether or not to add in overall mean voltage (across probes). This is subtracted in
+%       many of the raw-data files
+%   filt_cutoff: cutoff frequencies of bandpass filtering (default is no filtering). Use nan to
+%       indicate no filtering
+%   use_chs: Vector of channels to load (default is ch 1).
+% OUTPUTS:
+%   V: Matrix of voltage values (Time x ch.)
+%   Vtime: vector of corresponding timestamps
+%   Fs: scalar giving the sample frequency
 
 if nargin < 2 || isempty(add_Vmean)
-   add_Vmean = true; 
+    add_Vmean = true;
 end
 if nargin < 3 || isempty(filt_cutoff)
     filt_cutoff = [nan nan];
@@ -12,10 +25,8 @@ end
 
 load(name);
 
-V = double(FullV.V(use_chs,:))';
-if isfield(FullV,'intscale')
-lfp_int2V = FullV.intscale(1)/FullV.intscale(2);
-end
+V = double(FullV.V(use_chs,:))'; %grab desired channels and convert to double
+%if we're adding the overall (across-probe) avg back into the signal
 if add_Vmean
     first_dot = find(name == '.',1);
     Vmean_fname = [name(1:(first_dot-1)) 'FullVmean.mat'];
@@ -25,29 +36,34 @@ if add_Vmean
     load(Vmean_fname);
     V = V + FullV.sumscale*sumv;
 end
+
+%convert to voltage if the conversion is available
 if isfield(FullV,'intscale')
-V = V*lfp_int2V;
+    lfp_int2V = FullV.intscale(1)/FullV.intscale(2);
+    V = V*lfp_int2V;
 end
 
-Fs = 1/FullV.samper;
+Fs = 1/FullV.samper; %get sample-frequency
 
+%apply any filtering using butterworth 2/4-pole
 if any(~isnan(filt_cutoff))
     niqf = Fs/2;
-   if all(~isnan(filt_cutoff)) %bandpass
-       [bb,aa] = butter(2,filt_cutoff/niqf);
-   elseif ~isnan(filt_cutoff(1))
-       [bb,aa] = butter(2,filt_cutoff(1)/niqf,'high');
-   elseif ~isnan(filt_cutoff(2))
-       [bb,aa] = butter(2,filt_cutoff(2)/niqf,'low');
-   end
-   V = filtfilt(bb,aa,V);
+    if all(~isnan(filt_cutoff)) %bandpass
+        [bb,aa] = butter(2,filt_cutoff/niqf);
+    elseif ~isnan(filt_cutoff(1)) %high-pass
+        [bb,aa] = butter(2,filt_cutoff(1)/niqf,'high');
+    elseif ~isnan(filt_cutoff(2)) %low-pass
+        [bb,aa] = butter(2,filt_cutoff(2)/niqf,'low');
+    end
+    V = filtfilt(bb,aa,V); %symmetric filter
 end
 
+%get vector of timestamps if requested
 if nargout > 1
-    first = 1;
+    first = 1; 
     Vtime = nan(size(V,1),1);
-    for j = 1:length(FullV.blklen)
-        last = first+FullV.blklen(j)-1;
+    for j = 1:length(FullV.blklen) %loop over data chunks
+        last = first+FullV.blklen(j)-1; %last index of chunk
         Vtime(first:last) = FullV.blkstart(j)+[1:FullV.blklen(j)].*FullV.samper;
         first = last+1;
     end
@@ -56,9 +72,9 @@ else
     bad_pts = [];
 end
 
+%eliminate any bad samples
 if ~isempty(bad_pts)
     %     fprintf('Eliminating %d of %d bad V samples\n',length(bad_pts),length(Vtime));
-    
     V(bad_pts,:) = [];
     Vtime(bad_pts) = [];
 end
--- run_initial_clustering.m
+++ run_initial_clustering.m
@@ -1,3 +1,8 @@
+%{
+This script runs an initial clustering algorithm on all channels in a given directory, on a
+specified set of recording blocks
+%}
+
 clear all
 close all
 addpath('~/autocluster/'); 
@@ -32,11 +37,11 @@ Vloaded = nan; %keeps track of which raw data file is currently loaded (Nan mean
 
 %% SET CLUSTERING PARAMETERS
 clear clust_params
-clust_params.gmm_inits = 100; %number of GMM random inits
-clust_params.min_Pcomp = 0.005; %minimum component probability
-clust_params.use_ms = 0; %mean-subtraction on templates
-clust_params.try_features = [1 2 4]; %features to use [PCs Voltage Energy Templates]
-clust_params.max_back_comps = 2; %maximum N comps for modeling background
+clust_params.gmm_inits = 100; %number of GMM random initializations (more inits decreases likelihood of hitting inferior local minima)
+clust_params.min_Pcomp = 0.005; %minimum component probability (having this non-zero helps prevent outlier waveforms from creating their own clusters)
+clust_params.use_ms = false; %use mean-subtraction on templates (control whether this is a dot-product, or a correlation based similarity measure)
+clust_params.try_features = [1 2 4]; %features to use from the set: [PCs Voltage Energy Templates]
+clust_params.max_back_comps = 2; %maximum N components for modeling background spikes
 clust_params.cluster_bias = 0.5; %minimum posterior prob to assign to non-background cluster
 clust_params.target_rate = 50; %target rate for spike detection
 
@@ -47,6 +52,7 @@ if strcmp(rec_type,'UA')
 elseif strcmp(rec_type,'LP')
     n_probes = 24;
 end
+target_probes = 1:n_probes; %default use all probes
 
 %LOOK AT DURATION OF EACH EXPERIMENT BLOCK AS A WAY TO PICK A SET OF BASE BLOCKS FOR INITIAL CLUSTERING
 load(sprintf('%s/%s%sExpts.mat',expt_file_loc,monk_name,Expt_name));
@@ -76,58 +82,64 @@ if strcmp(rec_type,'LP');
     plot(1:n_blocks,ed,'o-');
     xlabel('Block number');
     ylabel('Electrode depth (mm)');
-xlim([0 n_blocks+1])
+    xlim([0 n_blocks+1])
 end
 
 %get set of blocks to run initial cluster fitting on
 poss_base_blocks = input('Enter vector of block numbers to fit initial models on\n');
 assert(all(ismember(poss_base_blocks,1:n_blocks)),'inputs must be possible recording blocks');
-fprintf('Fitting initial models on blocks %s\n',sprintf('%d ',poss_base_blocks));
-target_probes = 1:n_probes;
+
+%the actual numbers of the blocks in the data files might be different, so get these numbers
 if isfield(Expts{1}.Header,'exptno')
-raw_block_nums = cellfun(@(X) X.Header.exptno,Expts,'uniformoutput',1); %block numbering for EM/LFP data sometimes isnt aligned with Expts struct
+    raw_block_nums = cellfun(@(X) X.Header.exptno,Expts,'uniformoutput',1); %block numbering for EM/LFP data sometimes isnt aligned with Expts struct
 else
     raw_block_nums = 1:n_blocks;
 end
+
 %% PERFORM INITIAL CLUSTERING
+fprintf('Fitting initial models on blocks %s ...\n',sprintf('%d ',poss_base_blocks));
+fprintf('Using %d probes ...\n',length(target_probes));
+block_all_dprimes = nan(length(poss_base_blocks,length(target_probes)));
 for bb = 1:length(poss_base_blocks) %loop over initial set of blocks
-    cur_base_block = poss_base_blocks(bb);
-    cur_raw_block = raw_block_nums(cur_base_block);
+    cur_base_block = poss_base_blocks(bb); %current block num
+    cur_raw_block = raw_block_nums(cur_base_block); %number of current block in raw data file
     
-    full_dat_name = [base_save_dir sprintf('/Block%d_Clusters.mat',cur_base_block)];
-    second_dat_name = [base_save_dir sprintf('/Block%d_initClusters.mat',cur_base_block)];
-    if exist(full_dat_name,'file');
+    full_dat_name = [base_save_dir sprintf('/Block%d_Clusters.mat',cur_base_block)]; %name of cluster file
+    second_dat_name = [base_save_dir sprintf('/Block%d_initClusters.mat',cur_base_block)]; %create a copy of the cluster file to keep track of the initial clustering
+    
+    if exist(full_dat_name,'file'); %if there is already a file by this name, load in these clusters
         fprintf('Loading clusters for block %d\n',cur_base_block);
         load(full_dat_name,'Clusters');
-    else
+    else %otherwise initialize new clusters
         fprintf('Initiating new cluster block\n');
         Clusters = cell(n_probes,1);
     end
     
-    all_probe_fig = figure('visible','off');
-    n_cols = ceil(sqrt(n_probes)); n_rows = ceil(n_probes/n_cols);
-    for probe_num = target_probes 
+    all_probe_fig = figure('visible','off'); %initialize figure showing clustering on all probes
+    n_cols = ceil(sqrt(n_probes)); n_rows = ceil(n_probes/n_cols); %number of subplots
+    
+    for probe_num = target_probes %loop over each target probe and fit initial cluster model
        fprintf('\nClustering probe %d of %d\n',probe_num,n_probes);
         
-        if Expt_name(1) == 'G' %for UTAH array data Load in Voltage signasl for each probe
+        if Expt_name(1) == 'G' %for UTAH array data Load in Voltage signals for each probe
             loadedData = [data_dir sprintf('/Expt%d.p%dFullV.mat',cur_raw_block,probe_num)];
             use_chs = [];
         else %for Laminar probe data load in all voltage signals for a given block
             sfile_name = [data_dir sprintf('/Expt%dFullV.mat',cur_raw_block)];
-            use_chs = [probe_num-1 probe_num probe_num + 1];
-            use_chs(use_chs < 1 | use_chs > n_probes) = [];
-%             use_chs = probe_num;
-            if Vloaded ~= cur_raw_block
+            use_chs = [probe_num-1 probe_num probe_num + 1]; %use signals on the two nearest channels as well
+            use_chs(use_chs < 1 | use_chs > n_probes) = []; 
+            if Vloaded ~= cur_raw_block %if the data for this block isn't already loaded
                 fprintf('Loading data file %s\n',sfile_name);
                 [loadedData.V,loadedData.Vtime,loadedData.Fs] = Load_FullV(sfile_name, false, [100 nan],1:n_probes);
                 Vloaded = cur_raw_block;
             end
         end
         
-        %DETECT SPIKES AND PERFORM INITIAL CLUSTERING (2 CLUSTERS)
+        %DETECT SPIKES AND PERFORM INITIAL CLUSTERING (FITS 2 CLUSTERS)
         [cluster_details,spike_features,sum_fig] = detect_and_cluster_init(loadedData,clust_params,use_chs);
         cluster_details.base_block = cur_base_block;
-        Clusters{probe_num} = cluster_details;
+        
+        Clusters{probe_num} = cluster_details; %save cluster into Cluster array
         fprintf('Saving cluster details\n');
         save(full_dat_name,'Clusters');
         save(second_dat_name,'Clusters');
@@ -139,22 +151,23 @@ for bb = 1:length(poss_base_blocks) %loop over initial set of blocks
         close(sum_fig);
 
         %add to all probe plot
-        spike_xy = spike_features*cluster_details.xy_projmat;
+        spike_xy = spike_features*cluster_details.xy_projmat; %get xy-features of spikes
         N_spks = size(spike_xy,1);
-        su_inds = find(cluster_details.spike_clusts == 2);
-        mu_inds = find(cluster_details.spike_clusts == 1);        
+        su_inds = find(cluster_details.spike_clusts == 2); %indices of SU spikes
+        mu_inds = find(cluster_details.spike_clusts == 1); %indices of MU spikes
         set(0,'CurrentFigure',all_probe_fig);
         subplot(n_cols,n_rows,probe_num);hold on
         plot(spike_xy(mu_inds,1),spike_xy(mu_inds,2),'k.','markersize',1);
         plot(spike_xy(su_inds,1),spike_xy(su_inds,2),'r.','markersize',1);
         set(gca,'xtick',[],'ytick',[]);axis tight
-        for ii = 1:length(cluster_details.cluster_labels)
+        for ii = 1:length(cluster_details.cluster_labels) %add gaussian contours 
             h1 = plot_gaussian_2d(cluster_details.gmm_xyMeans(ii,:)',squeeze(cluster_details.gmm_xySigma(:,:,ii)),[2],'b',1);
         end
         title(sprintf('P%d',probe_num),'color','r');
         
     end
     
+    %save all-probe plot
     fillPage(all_probe_fig,'papersize',[14 14]);
     pname = [base_save_dir sprintf('/Allprobe_Block%d_scatter',cur_base_block)];
     print(all_probe_fig,pname,'-dpng');
@@ -172,11 +185,11 @@ for bb = 1:length(poss_base_blocks) %loop over initial set of blocks
 %     print(pname,'-dpng');
 %     close(gcf);
     
-    block_all_dprimes(bb,:) = cellfun(@(x) x.dprime,Clusters);
+    block_all_dprimes(bb,:) = cellfun(@(x) x.dprime,Clusters); %save cluster d-primes
     
 end
 
-%% INITIALIZE REF CLUSTERS
+%% INITIALIZE REFERENCE CLUSTERS BASED ON INITIAL CLUSTER FITS
 fprintf('Saving REFCLUSTERS details\n');
 [best_dprimes,best_blocks] = nanmax(block_all_dprimes,[],1);
 %NOTE, you can manually change the reference block used for each probe here
