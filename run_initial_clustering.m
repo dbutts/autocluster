13f02cdaf30cd89e3c0325d6f2ef3f32c0e62d32
James <james@CA1.(none)>
2015-11-30 13:49:55 -0500

    Test commit

--- run_initial_clustering.m
+++ run_initial_clustering.m
@@ -1,24 +1,21 @@
 clear all
 close all
-addpath('~/James_scripts/autocluster/');
+addpath('~/autocluster/'); 
 
 global data_dir base_save_dir init_save_dir Expt_name Vloaded n_probes loadedData raw_block_nums
-Expt_name = 'M320';
-monk_name = 'lem';
-rec_type = 'LP'; %or UA
+
+data_loc = '/media/NTlab_data3/Data/bruce/'; %directory containing raw data
+Expt_name = 'M012'; %short name of experiment
+monk_name = 'jbe'; %(lem and jbe are the two monkeys so far)
+rec_type = 'LP'; %type of recording (LP for laminar probe, or UA for Utah array)
 rec_number = 1;
 
-% data_loc = '/media/NTlab_data2/Data/bruce/';
-data_loc = '/media/NTlab_data3/Data/bruce/';
-% data_loc = '/home/james/Data/bruce/';
-
-
+%directory for saving clustering output
 base_save_dir = ['~/Analysis/bruce/' Expt_name '/clustering'];
 if rec_number > 1 %if you're splitting the recording into multiple separate chunks for clustering
    base_save_dir = [base_save_dir sprintf('/rec%d',rec_number)]; 
 end
-init_save_dir = [base_save_dir '/init'];
-
+init_save_dir = [base_save_dir '/init']; %subdir for initial cluster fits
 if ~exist(base_save_dir,'dir');
     mkdir(base_save_dir);
 end
@@ -29,49 +26,10 @@ end
 %location of FullV files
 data_dir = [data_loc Expt_name];
 
-%location of Expts.mat files
-data_dir2 = ['/media/NTlab_data3/Data/bruce/' Expt_name];
+expt_file_loc = ['/media/NTlab_data3/Data/bruce/' Expt_name]; %location of Expts.mat files (sometimes not in same raw-data dir)
 
-Vloaded = nan;
-%% LOOK AT DURATION OF EACH EXPERIMENT BLOCK AS A WAY TO PICK A SET OF BASE BLOCKS FOR INITIAL CLUSTERING
-cd(data_dir2);
-if strcmp(Expt_name,'G029')
-    load('G029Expts.mat');
-else
-    load(sprintf('%s%sExpts.mat',monk_name,Expt_name));
-end
-if strcmp(rec_type,'UA')
-    n_probes = 96;
-elseif strcmp(rec_type,'LP')
-    n_probes = 24;
-end
+Vloaded = nan; %keeps track of which raw data file is currently loaded (Nan means none loaded)
 
-n_blocks = length(Expts);
-[block_durs,ed] = deal(nan(n_blocks,1));
-for ii = 1:n_blocks
-    if ~isempty(Expts{ii})
-        trial_durs = [Expts{ii}.Trials(:).dur];
-        expt_durs(ii) = (Expts{ii}.Header.End - Expts{ii}.Header.Start)/1e4;
-        sum_trial_durs(ii) = nansum(trial_durs)/1e4;
-        ed(ii) = Expts{ii}.Stimvals.ed;
-    end
-end
-
-figure
-plot(expt_durs,'o-');
-hold on
-plot(sum_trial_durs,'ro-');
-
-figure;
-plot(ed,'o-');
-%%
-poss_base_blocks = [4 13 22 31]; %set of blocks to try fitting initial models on
-target_probes = 1:n_probes;
-if isfield(Expts{1}.Header,'exptno')
-raw_block_nums = cellfun(@(X) X.Header.exptno,Expts,'uniformoutput',1); %block numbering for EM/LFP data sometimes isnt aligned with Expts struct
-else
-    raw_block_nums = 1:n_blocks;
-end
 %% SET CLUSTERING PARAMETERS
 clear clust_params
 clust_params.gmm_inits = 100; %number of GMM random inits
@@ -81,6 +39,56 @@ clust_params.try_features = [1 2 4]; %features to use [PCs Voltage Energy Templa
 clust_params.max_back_comps = 2; %maximum N comps for modeling background
 clust_params.cluster_bias = 0.5; %minimum posterior prob to assign to non-background cluster
 clust_params.target_rate = 50; %target rate for spike detection
+
+%% PICK BLOCKS TO RUN INITIAL CLUSTER FITTING ON
+%set number of probes based on recording type
+if strcmp(rec_type,'UA')
+    n_probes = 96;
+elseif strcmp(rec_type,'LP')
+    n_probes = 24;
+end
+
+%LOOK AT DURATION OF EACH EXPERIMENT BLOCK AS A WAY TO PICK A SET OF BASE BLOCKS FOR INITIAL CLUSTERING
+load(sprintf('%s/%s%sExpts.mat',expt_file_loc,monk_name,Expt_name));
+n_blocks = length(Expts);
+[block_durs,ed] = deal(nan(n_blocks,1));
+for ii = 1:n_blocks
+    if ~isempty(Expts{ii})
+        trial_durs = [Expts{ii}.Trials(:).dur];
+        expt_durs(ii) = (Expts{ii}.Header.End - Expts{ii}.Header.Start)/1e4; %duration of recording block
+        sum_trial_durs(ii) = nansum(trial_durs)/1e4; %summed duration of trials
+        ed(ii) = Expts{ii}.Stimvals.ed;
+    end
+end
+
+%make figure showing recording duration and electrode depth (if applicable) across recording blocks
+figure;
+if strcmp(rec_type,'LP'); subplot(2,1,1); end;
+plot(1:n_blocks,expt_durs,'o-');
+hold on
+plot(1:n_blocks,sum_trial_durs,'ro-');
+xlabel('Block number');
+ylabel('Duration (s)');
+legend('Recording dur','Total trial dur');
+xlim([0 n_blocks+1])
+if strcmp(rec_type,'LP');
+    subplot(2,1,2);
+    plot(1:n_blocks,ed,'o-');
+    xlabel('Block number');
+    ylabel('Electrode depth (mm)');
+xlim([0 n_blocks+1])
+end
+
+%get set of blocks to run initial cluster fitting on
+poss_base_blocks = input('Enter vector of block numbers to fit initial models on\n');
+assert(all(ismember(poss_base_blocks,1:n_blocks)),'inputs must be possible recording blocks');
+fprintf('Fitting initial models on blocks %s\n',sprintf('%d ',poss_base_blocks));
+target_probes = 1:n_probes;
+if isfield(Expts{1}.Header,'exptno')
+raw_block_nums = cellfun(@(X) X.Header.exptno,Expts,'uniformoutput',1); %block numbering for EM/LFP data sometimes isnt aligned with Expts struct
+else
+    raw_block_nums = 1:n_blocks;
+end
 %% PERFORM INITIAL CLUSTERING
 for bb = 1:length(poss_base_blocks) %loop over initial set of blocks
     cur_base_block = poss_base_blocks(bb);
